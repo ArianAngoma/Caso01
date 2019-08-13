9ced7460d637ed2d1fa635acbbd64e2ec08d52cd
angoma850 <arian.angoma@tecsup.edu.pe>
2019-08-12 22:05:01 -0500

    asd

---
+++ CasoPropuesto_01/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CasoPropuesto_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dg1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dg1
+            // 
+            this.dg1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg1.Location = new System.Drawing.Point(25, 115);
+            this.dg1.Name = "dg1";
+            this.dg1.Size = new System.Drawing.Size(742, 239);
+            this.dg1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NOMBRE DEL CLIENTE";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(170, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(480, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(342, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "LISTADO DE CLIENTES";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(572, 387);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "TOTAL DE CLIENTES";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(715, 387);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(42, 13);
+            this.lbl1.TabIndex = 5;
+            this.lbl1.Text = "TOTAL";
+            this.lbl1.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dg1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dg1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ CasoPropuesto_01/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CasoPropuesto_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["CasoPropuesto_01"].ConnectionString);
+
+        public void ListaClientes()
+        {
+            
+            using(SqlDataAdapter df = new SqlDataAdapter("Usp_Caso_Primero", cn))
+            {
+                df.SelectCommand.CommandType = CommandType.StoredProcedure;
+                using (DataSet Da = new DataSet())
+                {
+                    df.Fill(Da, "Clientes");
+                    dg1.DataSource = Da.Tables["Clientes"];
+                    lbl1.Text = Da.Tables["Clientes"].Rows.Count.ToString();
+                }
+            }
+        }
+
+        public void BuscarCliente()
+        {
+            string variable = textBox1.Text;
+
+            using (SqlDataAdapter df = new SqlDataAdapter("Usp_Caso_Busqueda_Primero", cn))
+            {
+                df.SelectCommand.CommandType = CommandType.StoredProcedure;
+                using (DataSet Da2 = new DataSet())
+                {
+                    df.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    SqlParameter sqlParameter = new SqlParameter();
+                    sqlParameter.SqlDbType = SqlDbType.VarChar;
+                    sqlParameter.SqlValue = variable;
+                    sqlParameter.Size = 100;
+                    sqlParameter.ParameterName = "@idCliente";
+
+
+                    df.SelectCommand.Parameters.Add(sqlParameter);
+
+                    using (DataSet Da = new DataSet())
+                    {
+                        df.Fill(Da, "Clientes");
+                        dg1.DataSource = Da.Tables["Clientes"];
+                        lbl1.Text = Da.Tables["Clientes"].Rows.Count.ToString();
+                    }
+                }
+            } 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaClientes();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            BuscarCliente();
+        }
+    }
+}
